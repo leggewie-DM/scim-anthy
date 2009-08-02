35923e6ad4ceecf232f0b81be87e491e439a075f
Rolf Leggewie <foss@rolf.leggewie.biz>
2009-08-02 03:19:47 +0200

    Imported Upstream version 0.8.0

---
+++ src/scim_anthy_key2kana.cpp
@@ -0,0 +1,262 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2004 Hiroyuki Ikezoe
+ *  Copyright (C) 2004 Takuro Ashie
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "scim_anthy_key2kana.h"
+#include "scim_anthy_factory.h"
+#include "scim_anthy_imengine.h"
+#include "scim_anthy_utils.h"
+
+using namespace scim_anthy;
+
+Key2KanaConvertor::Key2KanaConvertor (AnthyInstance    & anthy,
+                                      Key2KanaTableSet & tables)
+    : m_anthy          (anthy),
+      m_tables         (tables)
+{
+    set_case_sensitive (false);
+}
+
+Key2KanaConvertor::~Key2KanaConvertor ()
+{
+}
+
+bool
+Key2KanaConvertor::can_append (const KeyEvent & key)
+{
+    // ignore key release.
+    if (key.is_key_release ())
+        return false;
+
+    // ignore short cut keys of apllication.
+    if (key.mask & SCIM_KEY_ControlMask ||
+        key.mask & SCIM_KEY_AltMask)
+    {
+        return false;
+    }
+
+    if (isprint(key.get_ascii_code ()) && !isspace(key.get_ascii_code ()))
+        return true;
+
+    if (util_key_is_keypad (key))
+        return true;
+
+    return false;
+}
+
+bool
+Key2KanaConvertor::append (const KeyEvent & key,
+                           WideString & result,
+                           WideString & pending,
+                           String &raw)
+{
+    if (!can_append (key))
+        return false;
+
+    util_keypad_to_string (raw, key);
+
+    if (util_key_is_keypad (key)) {
+        bool retval = false;
+        WideString wide;
+        String ten_key_type = m_anthy.get_factory()->m_ten_key_type;
+
+        // convert key pad string to wide
+        if ((ten_key_type == "FollowMode" &&
+             (m_anthy.get_input_mode () == SCIM_ANTHY_MODE_LATIN ||
+              m_anthy.get_input_mode () == SCIM_ANTHY_MODE_HALF_KATAKANA)) ||
+            ten_key_type == "Half")
+        {
+            wide = utf8_mbstowcs (raw);
+        } else {
+            util_convert_to_wide (wide, raw);
+        }
+
+        // join to previous string
+        if (!m_exact_match.is_empty()) {
+            if (!m_exact_match.get_result(0).empty() &&
+                m_exact_match.get_result(1).empty())
+            {
+                result = utf8_mbstowcs (m_exact_match.get_result(0));
+            } else {
+                retval = true; /* commit prev pending */
+            }
+            result += wide;
+        } else {
+            if (m_pending.length () > 0)
+                retval = true; /* commit prev pending */
+            result = wide;
+        }
+
+        m_pending.clear ();
+        m_exact_match.clear ();
+
+        return retval;
+
+    } else {
+        // the key isn't keypad
+        return append (raw, result, pending);
+    }
+}
+
+bool
+Key2KanaConvertor::append (const String & str,
+                           WideString & result, WideString & pending)
+{
+    WideString widestr = utf8_mbstowcs (str);
+    WideString matching_str = m_pending + widestr;
+    Key2KanaRule exact_match;
+    bool has_partial_match = false;
+    bool retval = false;
+
+    if (!m_case_sensitive) {
+        String half = utf8_wcstombs (matching_str);
+        for (unsigned int i = 0; i < half.length (); i++)
+            half[i] = tolower (half[i]);
+        matching_str = utf8_mbstowcs (half);
+    }
+
+    /* find matched table */
+    std::vector<Key2KanaTable*> &tables = m_tables.get_tables();
+    for (unsigned int j = 0; j < tables.size(); j++) {
+        if (!tables[j])
+            continue;
+
+        Key2KanaRules &rules = tables[j]->get_table ();
+
+        for (unsigned int i = 0; i < rules.size(); i++) {
+            /* matching */
+            String seq = rules[i].get_sequence ();
+            if (!m_case_sensitive) {
+                for (unsigned int j = 0; j < seq.length (); j++)
+                    seq[j] = tolower (seq[j]);
+            }
+            WideString romaji = utf8_mbstowcs(seq);
+            if (romaji.find (matching_str) == 0) {
+                if (romaji.length () == matching_str.length ()) {
+                    /* exact match */
+                    exact_match = rules[i];
+                } else {
+                    /* partial match */
+                    has_partial_match = true;
+                }
+            }
+        }
+    }
+
+    /* return result */
+    if (has_partial_match) {
+        m_exact_match = exact_match;
+        result.clear ();
+        m_pending += widestr;
+        pending   =  m_pending;
+
+    } else if (!exact_match.is_empty()) {
+        if (!exact_match.get_result(1).empty())
+            m_exact_match = exact_match;
+        else
+            m_exact_match.clear ();
+        m_pending = utf8_mbstowcs (exact_match.get_result (1));
+        result    = utf8_mbstowcs (exact_match.get_result (0));
+        pending   = m_pending;
+
+    } else {
+        if (!m_exact_match.is_empty()) {
+            if (!m_exact_match.get_result(0).empty() &&
+                m_exact_match.get_result(1).empty())
+            {
+                result = utf8_mbstowcs (m_exact_match.get_result(0));
+            } else {
+                retval = true; /* commit prev pending */
+            }
+            m_pending.clear ();
+            m_exact_match.clear ();
+
+            WideString tmp_result;
+            append(str, tmp_result, pending);
+            result += tmp_result;
+
+        } else {
+            if (m_pending.length () > 0) {
+                retval     = true; /* commit prev pending */
+                m_pending  = widestr;
+                pending    = m_pending;
+
+            } else {
+                result     = widestr;
+                pending.clear();
+                m_pending.clear ();
+            }
+        }
+    }
+
+    return retval;
+}
+
+void
+Key2KanaConvertor::clear (void)
+{
+    m_pending.clear ();
+    m_exact_match.clear ();
+}
+
+bool
+Key2KanaConvertor::is_pending (void)
+{
+    if (m_pending.length () > 0)
+        return true;
+    else
+        return false;
+}
+
+WideString
+Key2KanaConvertor::get_pending (void)
+{
+    return m_pending;
+}
+
+WideString
+Key2KanaConvertor::flush_pending (void)
+{
+    WideString result;
+    if (!m_exact_match.is_empty ()) {
+        if (!m_exact_match.get_result(0).empty() &&
+            m_exact_match.get_result(1).empty())
+        {
+            result = utf8_mbstowcs (m_exact_match.get_result(0));
+        } else if (!m_exact_match.get_result(1).empty()) {
+            result += utf8_mbstowcs (m_exact_match.get_result(1));
+        } else if (m_pending.length () > 0) {
+            result += m_pending;
+        }
+    }
+    clear ();
+    return result;
+}
+
+void
+Key2KanaConvertor::reset_pending (const WideString &result, const String &raw)
+{
+    for (unsigned int i = 0; i < raw.length (); i++) {
+        WideString res, pend;
+        append (raw.substr(i, 1), res, pend);
+    }
+}
+/*
+vi:ts=4:nowrap:ai:expandtab
+*/
---
+++ src/scim_anthy_key2kana.h
@@ -0,0 +1,75 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2004 Hiroyuki Ikezoe
+ *  Copyright (C) 2004 Takuro Ashie
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __SCIM_ANTHY_KEY2KANA_H__
+#define __SCIM_ANTHY_KEY2KANA_H__
+
+#define Uses_SCIM_EVENT
+#include <scim.h>
+
+#include "scim_anthy_key2kana_base.h"
+#include "scim_anthy_default_tables.h"
+#include "scim_anthy_key2kana_table.h"
+
+using namespace scim;
+
+class AnthyInstance;
+
+namespace scim_anthy {
+
+class Key2KanaConvertor : public Key2KanaConvertorBase
+{
+public:
+    Key2KanaConvertor             (AnthyInstance    & anthy,
+                                   Key2KanaTableSet & tables);
+    virtual ~Key2KanaConvertor    ();
+
+    bool       can_append         (const KeyEvent   & key);
+    bool       append             (const KeyEvent   & key,
+                                   WideString       & result,
+                                   WideString       & pending,
+                                   String           & raw);
+    void       clear              (void);
+
+    bool       is_pending         (void);
+    WideString get_pending        (void);
+    WideString flush_pending      (void);
+    void       reset_pending      (const WideString & result,
+                                   const String     & raw);
+
+private:
+    bool       append             (const String     & str,
+                                   WideString       & result,
+                                   WideString       & pending);
+
+private:
+    AnthyInstance     &m_anthy;
+    Key2KanaTableSet  &m_tables;
+
+    // state
+    WideString         m_pending;
+    Key2KanaRule       m_exact_match;
+};
+
+}
+#endif /* __SCIM_ANTHY_KEY2KANA_H__ */
+/*
+vi:ts=4:nowrap:ai:expandtab
+*/
